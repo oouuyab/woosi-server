e6bb607f7ff847dc8887adcada171f6642a9c44d
oouuyab <oouuyab@gmail.com>
2022-10-14 05:04:45 +0900

    feat: [DB] database 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
@@ -12,6 +13,16 @@ import { AppService } from './app.service';
           : process.env.NODE_ENV === 'test'
           ? '.test.env'
           : '.env',
+    }),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
     }),
   ],
   controllers: [AppController],
