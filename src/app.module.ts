e206b24a1fdf17f211226ad831d989b8843c0aa1
oouuyab <oouuyab@gmail.com>
2022-10-14 04:49:11 +0900

    feat: [Server] env 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath:
+        process.env.NODE_ENV === 'prod'
+          ? '.prod.env'
+          : process.env.NODE_ENV === 'test'
+          ? '.test.env'
+          : '.env',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
