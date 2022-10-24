3a67f7811e4e565f52567df8f73b4fe9d216ea69
oouuyab <oouuyab@gmail.com>
2022-10-24 17:36:38 +0900

    feat: [User] enum & error-msg 추가

---
+++ src/common/enum.ts
@@ -0,0 +1,10 @@
+export enum USER_TYPE {
+  DEFAULT = 0,
+  HOST = 1,
+}
+
+export enum USER_STATUS {
+  ACTIVE = 1,
+  INACTIVE = 2,
+  LEAVE = 3,
+}
---
+++ src/common/error-msg.ts
@@ -0,0 +1,6 @@
+export const ERROR_MSG = {
+  ALREADY_EXIST_USER: '이미 가입한 유저입니다.',
+  LEAVE_USER: '탈퇴한 유저입니다.',
+  INACTIVE_USER: '휴면 유저입니다.',
+  NOT_FOUND_USER: '가입한 유저를 찾을 수 없습니다.',
+};
