7dcb9ab4b18bb7e18b4b893093e29bda10b9ddd7
oouuyab <oouuyab@gmail.com>
2022-10-24 18:58:52 +0900

    feat: [Server] swagger 설정

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('Woosi Swagger')
+    .setDescription('The Woosi API description')
+    .setVersion('0.1')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/swagger', app, document);
+
   await app.listen(process.env.PORT);
 }
 bootstrap();
