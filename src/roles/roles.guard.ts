96eaa6728ad08af84f266dc5d97a88864a8632f1
oouuyab <oouuyab@gmail.com>
2022-11-24 02:44:41 +0900

    refactor: [Role] roles.guard.ts에 불필요하게 import한 부분 삭제

--- src/roles/roles.guard.ts
+++ src/roles/roles.guard.ts
@@ -1,6 +1,5 @@
 import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
 import { ROLE } from '../common/enum';
 
 @Injectable()
